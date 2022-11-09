f064d5795513c33b264a30cc0853b6f799e5f746
Helter Xavier <helter.xavier@hotmail.com>
2022-11-09 11:19:11 -0300

    0911

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu')
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
+        document.querySelector('.iconToggle').src='/assets/imgs/x.png'
     }else {
         menuMobile.classList.add('open');
+        document.querySelector('.iconToggle').src = "/assets/imgs/menu-aberto.png";
     }
 }
