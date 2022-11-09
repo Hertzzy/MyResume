f095692692f35df686db427208aae3269bc271e0
Helter Xavier <helter.xavier@hotmail.com>
2022-11-09 11:32:57 -0300

    curriculo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
+//Menu Toggle
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu')
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
-        document.querySelector('.iconToggle').src='/assets/imgs/x.png'
+        document.querySelector('.iconToggle').src='/assets/imgs/menu-aberto.png'
     }else {
         menuMobile.classList.add('open');
-        document.querySelector('.iconToggle').src = "/assets/imgs/menu-aberto.png";
+        document.querySelector('.iconToggle').src = "/assets/imgs/x.png";
     }
 }
+
+//Dowdownload CV
+const download = () => {
+    fetch('CurriculoHelter.pdf').then(response => {
+        response.blob().then(blob =>{
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'SamplePDF.pdf';
+            alink.click();
+        })
+    })
+ }
