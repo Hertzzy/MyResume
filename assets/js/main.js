92e30d9ec77dfc5598d8bd226fa34ddb0866e6a5
Helter Xavier <helter.xavier@hotmail.com>
2022-11-10 22:58:46 -0300

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,33 @@ const download = () => {
         })
     })
  }
+
+//Relogio
+//Selecionando as classes para manipular as horas
+const hours = document.querySelector('.horas');
+const minutes = document.querySelector('.minutos');
+const segundo = document.querySelector('.segundos');
+
+//Arrow funciton SetInterval
+const timerHour = setInterval(function time(){
+    let dateToday = new Date();
+    let hora = dateToday.getHours();
+    let min = dateToday.getMinutes();  
+    let sec = dateToday.getSeconds();  
+
+    if (hora < 10) {
+        hora = '0' + hora;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+
+    hours.textContent = hora;
+    minutes.textContent = min;
+    segundo.textContent = sec;
+})
+
+// console.log(hours, minutes, sec)
