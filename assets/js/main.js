3b8f18418df2dabfd7034358470b8a6653041597
Helter Xavier <helter.xavier@hotmail.com>
2022-11-23 18:44:39 -0300

    23/11

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 //Menu Toggle
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu')
-    if(menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open');
-        document.querySelector('.iconToggle').src='/assets/imgs/menu-aberto.png'
-    }else {
-        menuMobile.classList.add('open');
-        document.querySelector('.iconToggle').src = "/assets/imgs/x.png";
-    }
-}
+// function menuShow() {
+//     let menuMobile = document.querySelector('.mobile-menu')
+//     if(menuMobile.classList.contains('open')){
+//         menuMobile.classList.remove('open');
+//         document.querySelector('.iconToggle').src='/assets/imgs/menu-aberto.png'
+//     }else {
+//         menuMobile.classList.add('open');
+//         document.querySelector('.iconToggle').src = "/assets/imgs/x.png";
+//     }
+// }
 
 //Dowdownload CV
 const download = () => {
@@ -24,32 +24,35 @@ const download = () => {
     })
  }
 
-//Relogio
-//Selecionando as classes para manipular as horas
-const hours = document.querySelector('.horas');
-const minutes = document.querySelector('.minutos');
-const segundo = document.querySelector('.segundos');
+// //Relogio
+// //Selecionando as classes para manipular as horas
+// const hours = document.querySelector('.horas');
+// const minutes = document.querySelector('.minutos');
+// const segundo = document.querySelector('.segundos');
 
-//Arrow funciton SetInterval
-const timerHour = setInterval(function time(){
-    let dateToday = new Date();
-    let hora = dateToday.getHours();
-    let min = dateToday.getMinutes();  
-    let sec = dateToday.getSeconds();  
+// //Arrow funciton SetInterval
+// const timerHour = setInterval(function time(){
+//     let dateToday = new Date();
+//     let hora = dateToday.getHours();
+//     let min = dateToday.getMinutes();  
+//     let sec = dateToday.getSeconds();  
 
-    if (hora < 10) {
-        hora = '0' + hora;
-    }
-    if (min < 10) {
-        min = '0' + min;
-    }
-    if (sec < 10) {
-        sec = '0' + sec;
-    }
+//     if (hora < 10) {
+//         hora = '0' + hora;
+//     }
+//     if (min < 10) {
+//         min = '0' + min;
+//     }
+//     if (sec < 10) {
+//         sec = '0' + sec;
+//     }
 
-    hours.textContent = hora;
-    minutes.textContent = min;
-    segundo.textContent = sec;
-})
+//     hours.textContent = hora;
+//     minutes.textContent = min;
+//     segundo.textContent = sec;
+// })
 
 // console.log(hours, minutes, sec)
+
+
+
