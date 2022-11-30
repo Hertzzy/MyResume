cfaaade7611da6f9ddf6d91527597f534bc7a05a
Helter Xavier <helter.xavier@hotmail.com>
2022-11-30 20:59:10 -0300

    animate-scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,57 @@ const download = () => {
     })
  }
 
-function redirectCalc(){
-    location.replace('http://127.0.0.1:5500/projects/calc/calculadora.html')
+// function redirectCalc(){
+//     location.replace('http://127.0.0.1:5500/projects/calc/calculadora.html');
+// }
+
+//Animate Scroll
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+// 1 - Selecionar elementos que devem ser animados 
+const target = document.querySelectorAll('[data-anime]');
+
+// 2 - Definir a classe que é adicionada durante a animação
+const animationClass = 'animate';
+
+// 3 - Criar função de animação
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) /  4);
+    target.forEach(function(element) {
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        }else {
+            element.classList.remove(animationClass);
+        }
+        // console.log(element.offsetTop);
+    });
 }
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll', debounce(function(){
+        animeScroll();
+    }, 200))
+}
+
+
+// 3.1 - Verificar a distância entre a barra de Scroll e o topo do site
+// 3.2 - Verificar se a distancia de 3.1 + Offset é maior  do que a distância entre o elemento e o Topo da párgina
+// 3.3 - Se verdadeiro, adicionar classe  de animação, remover se for falso
+// 4 - Ativar a função de animação toda vez que o usuário utilizar o scroll
+// 5 - Otimizar ativação
